46eefe1204e90e32af1ca4e0af14b6156a2b85ec
Fleur Petit <fleur.petit@alliander.com>
2024-10-01 11:49:31 +0200

    patient normalise

--- inflammation/models.py
+++ inflammation/models.py
@@ -43,3 +43,20 @@ def daily_min(data):
     :returns: array of daily minimums
     """
     return np.min(data, axis=0)
+
+def patient_normalise(data):
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values error is raised.
+    """
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
+    max_rows = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max_rows[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
